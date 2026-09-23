80eeb2205cd46a3d9714109403894c52f803f511
Leoul Zerihun <leoulzerihun06@gmail.com>
2026-09-23 13:12:35 +0300

    add user model

--- Backend/users/admin.py
+++ Backend/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+# pyrefly: ignore [missing-import]
+from .models import User
 
-# Register your models here.
+admin.site.register(User, UserAdmin)
+
+
+
+
--- Backend/users/models.py
+++ Backend/users/models.py
@@ -1,3 +1,13 @@
+from enum import unique
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email= models.EmailField(unique=True)
+    avatar_color= models.CharField(max_length=7, default='#46F5E5')
+
+    USERNAME_FIELD="email"
+    REQUIRED_FIELDS=["username"]
+
+    def __str__(self):
+        return self.email
